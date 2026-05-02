e6e268d02f63e3c5173988ba22fe6f55b29d1afe
Colin Senner <colin.senner@gmail.com>
2026-05-02 00:26:27 -0700

    wip

--- install/servuo-install.sh
+++ install/servuo-install.sh
@@ -15,20 +15,16 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  curl \
-  git \
- 
-$STD add-apt-respoitory ppa:dotnet/backports
 $STD apt-get update
-$STD sudo apt-get -y install zlib1g mono-complete dotnet-sdk-10.0 dotnet-runtime-10.0
+$STD apt install -y git curl wget zlib1g
+$STD sudo apt -y install zlib1g mono-complete dotnet-sdk-10.0 dotnet-runtime-10.0
 
 msg_ok "Installed Dependencies"
 
-# msg_info "Downloading dotnet-install.sh script"
-# $STD curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
-# $STD chmod +x dotnet-install.sh
-# $STD ./dotnet-install.sh --channel 8.0
+msg_info "Downloading dotnet-install.sh script"
+$STD curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+$STD chmod +x dotnet-install.sh
+$STD ./dotnet-install.sh --channel 8.0
 
 msg_info "Cloning ServUO"
 $STD git clone --depth 1 https://github.com/ServUO/ServUO.git /opt/servuo
