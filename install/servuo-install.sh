89cff05812ce9ce946a6ab5b5239c48a776b146d
Colin Senner <colin.senner@gmail.com>
2026-05-07 00:08:38 -0700

    wip

--- install/servuo-install.sh
+++ install/servuo-install.sh
@@ -79,7 +79,7 @@ msg_ok "Downloaded UOS_Latest.exe"
 
 # Creating UOS_Latest.exe install script
 cat >install_uos.sh <<'EOF'
-#!/usr/bin/env bash
+"#!/usr/bin/env bash
 export WINEPREFIX=/opt/uo/
 export WINEARCH=win32
 
@@ -126,58 +126,58 @@ $STD xvfb-run ./install_uos.sh
 msg_ok "Installed UOS_Latest.exe"
 
 # dotnet
-msg_info "Installing dotnet SDK"
-$STD wget https://packages.microsoft.com/config/debian/13/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-$STD sudo dpkg -i packages-microsoft-prod.deb
-$STD rm packages-microsoft-prod.deb
-$STD apt update && apt install -y dotnet-sdk-10.0
-msg_ok "Installed dotnet SDK"
+# msg_info "Installing dotnet SDK"
+# $STD wget https://packages.microsoft.com/config/debian/13/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# $STD sudo dpkg -i packages-microsoft-prod.deb
+# $STD rm packages-microsoft-prod.deb
+# $STD apt update && apt install -y dotnet-sdk-10.0
+# msg_ok "Installed dotnet SDK"
 
-msg_info "Cloning ServUO"
-$STD git clone --depth 1 https://github.com/ServUO/ServUO.git ${APP_DIR}
-msg_ok "Cloned ServUO"
+# msg_info "Cloning ServUO"
+# $STD git clone --depth 1 https://github.com/ServUO/ServUO.git ${APP_DIR}
+# msg_ok "Cloned ServUO"
 
-msg_info "Copying UO Client Files to ${UO_DATA_DIR}"
-$STD mkdir ${UO_DATA_DIR}
-$STD cp "/opt/uo/drive_c/Program\ Files/Electronic\ Arts/Ultima\ Online\ Classic/*.mul" ${UO_DATA_DIR}/
-msg_ok "Copied UO Client Files"
+# msg_info "Copying UO Client Files to ${UO_DATA_DIR}"
+# $STD mkdir ${UO_DATA_DIR}
+# $STD cp "/opt/uo/drive_c/Program\ Files/Electronic\ Arts/Ultima\ Online\ Classic/*.mul" ${UO_DATA_DIR}/
+# msg_ok "Copied UO Client Files"
 
-msg_info "Setting the custom path in ServUO ${DATAPATH_CONFIG}"
-$STD sed -i "s|^#CustomPath=.*|CustomPath=${UO_DATA_DIR}|" ${DATAPATH_CONFIG}
+# msg_info "Setting the custom path in ServUO ${DATAPATH_CONFIG}"
+# $STD sed -i "s|^#CustomPath=.*|CustomPath=${UO_DATA_DIR}|" ${DATAPATH_CONFIG}
 
-msg_info "Creating Service"
-cat <<EOF >/etc/systemd/system/servuo.service
-[Unit]
-Description=ServUO Ultima Online Server
-After=network.target
+# msg_info "Creating Service"
+# cat <<EOF >/etc/systemd/system/servuo.service
+# [Unit]
+# Description=ServUO Ultima Online Server
+# After=network.target
 
-[Service]
-WorkingDirectory=${APP_DIR}
-ExecStart=/usr/bin/mono ${APP_DIR}/ServUO.exe
-Restart=always
+# [Service]
+# WorkingDirectory=${APP_DIR}
+# ExecStart=/usr/bin/mono ${APP_DIR}/ServUO.exe
+# Restart=always
 
-[Install]
-WantedBy=multi-user.target
-EOF
+# [Install]
+# WantedBy=multi-user.target
+# EOF
 
-systemctl enable -q --now servuo.service
-msg_ok "Created Service"
+# systemctl enable -q --now servuo.service
+# msg_ok "Created Service"
 
-msg_info "Building ServUO"
-$STD cd ${APP_DIR}
-$STD make build release
-msg_ok "Built ServUO"
+# msg_info "Building ServUO"
+# $STD cd ${APP_DIR}
+# $STD make build release
+# msg_ok "Built ServUO"
 
-motd_ssh
-customize
+# motd_ssh
+# customize
 
-cat >>/etc/motd <<EOF
+# cat >>/etc/motd <<EOF
 
- ServUO Port: 2593
- Docs: https://github.com/ServUO/ServUO/wiki
-EOF
+#  ServUO Port: 2593
+#  Docs: https://github.com/ServUO/ServUO/wiki
+# EOF
 
-msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
-msg_ok "Cleaned up"
+# msg_info "Cleaning up"
+# $STD apt-get -y autoremove
+# $STD apt-get -y autoclean
+# msg_ok "Cleaned up"
